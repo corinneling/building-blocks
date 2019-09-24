33fe2ee797820d85051b27102cee0bdecf6810e4
corinne <corinne@heysparkbox.com>
2019-09-23 21:43:31 -0400

    :coffee: update modal tests

--- src/spec/modal-spec.js
+++ src/spec/modal-spec.js
@@ -4,86 +4,82 @@ import { modalSetup, toggleHiddenValue } from '../js/modal/helpers';
 // TO DO: see if i can add more tests to this file, such as testing focus management
 
 describe('Modal', () => {
-  describe('modalSetup', () => {
-    beforeEach(() => {
-      const dom = new JSDOM(
-        `
-        <html>
-        <body class="no-js">
-          <button class="trigger-annoyingness">Click If You Dare!</button>
-          <div class="annoying" role="dialog" aria-hidden="false">
-             I'm a Modal!
-             <button class="close">Escape the Popup!</button>
-          </div>
-        </body>
-        </html>
-        `, {
-          url: 'http://localhost'
-        },
-      )
-      global.window = dom.window;
-      global.document = dom.window.document;
-    })
-
-    it('removes the no-js class', () => {
-      // Arrange
-      const body = document.querySelector('.no-js');
-      const annoyingModal = document.querySelector('.annoying');
-      // Act
-      modalSetup(body, annoyingModal);
-      // Assert
-      expect(body.outerHTML).to.not.include('no-js')
-    })
-  
-    it('set\'s the aria-hidden value to true', () => {
-      // Arrange
-      const body = document.querySelector('.no-js');
-      const annoyingModal = document.querySelector('.annoying');
-      // Act
-      modalSetup(body, annoyingModal);
-      // Assert
-      expect(annoyingModal.outerHTML).to.include('aria-hidden="true"')
-    })
+  beforeEach(() => {
+    const dom = new JSDOM(
+      `
+      <html>
+      <body class="no-js">
+        <button class="trigger-annoyingness">Click If You Dare!</button>
+        <div class="annoying" role="dialog" aria-hidden="false">
+            I'm a Modal!
+            <button class="close">Escape the Popup!</button>
+        </div>
+      </body>
+      </html>
+      `, {
+        url: 'http://localhost'
+      },
+    )
+    global.window = dom.window;
+    global.document = dom.window.document;
   })
 
-  describe('toggleHiddenValue', () => {
-    beforeEach(() => {
-      const dom = new JSDOM(
-        `
-        <html>
-        <body class="no-js">
-          <button class="trigger-annoyingness">Click If You Dare!</button>
-          <div class="annoying" role="dialog" aria-hidden="false">
-             I'm a Modal!
-             <button class="close">Escape the Popup!</button>
-          </div>
-        </body>
-        </html>
-        `, {
-          url: 'http://localhost'
-        },
-      )
-      global.window = dom.window;
-      global.document = dom.window.document;
-    })
+  afterEach(() => {
+    const dom = new JSDOM(
+      `
+      <html>
+      <body class="no-js">
+        <button class="trigger-annoyingness">Click If You Dare!</button>
+        <div class="annoying" role="dialog" aria-hidden="false">
+          I'm a Modal!
+          <button class="close">Escape the Popup!</button>
+        </div>
+      </body>
+      </html>
+      `, {
+        url: 'http://localhost'
+      },
+    )
+    global.window = dom.window;
+    global.document = dom.window.document;
+  })
 
-    it('hides the modal when originally visible ', () => {
-      // Arrange
-      const modal = document.querySelector('.annoying');
-      // Act
-      toggleHiddenValue(modal);
-      // Assert
-      expect(modal.outerHTML).to.include('aria-hidden="true"')
-    })
+  it('removes the no-js class', () => {
+    // Arrange
+    const body = document.querySelector('.no-js');
+    const annoyingModal = document.querySelector('.annoying');
+    // Act
+    modalSetup(body, annoyingModal);
+    // Assert
+    expect(body.outerHTML).to.not.include('no-js')
+  })
 
-    it('shows the modal when visible ', () => {
-      // Arrange
-      const modal = document.querySelector('.annoying');
-      // Act
-      toggleHiddenValue(modal);
-      toggleHiddenValue(modal);
-      // Assert
-      expect(modal.outerHTML).to.include('aria-hidden="false"')
-    })
+  it('set\'s the aria-hidden value to true', () => {
+    // Arrange
+    const body = document.querySelector('.no-js');
+    const annoyingModal = document.querySelector('.annoying');
+    // Act
+    modalSetup(body, annoyingModal);
+    // Assert
+    expect(annoyingModal.outerHTML).to.include('aria-hidden="true"')
+  })
+
+  it('hides the modal when originally visible ', () => {
+    // Arrange
+    const modal = document.querySelector('.annoying');
+    // Act
+    toggleHiddenValue(modal);
+    // Assert
+    expect(modal.outerHTML).to.include('aria-hidden="true"')
+  })
+
+  it('shows the modal when visible ', () => {
+    // Arrange
+    const modal = document.querySelector('.annoying');
+    // Act
+    toggleHiddenValue(modal);
+    toggleHiddenValue(modal);
+    // Assert
+    expect(modal.outerHTML).to.include('aria-hidden="false"')
   })
 })
